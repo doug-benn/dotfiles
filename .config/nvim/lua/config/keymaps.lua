035a2f06e64ca93fbfd278306e6025f64ac1f275
Douglas Benn <fiery.coder@gmail.com>
2025-11-02 20:25:12 +0000

    updated nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,25 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<c-z>", "<nop>", { silent = true, remap = true })
-
-local hardmode = false
-if hardmode then
-  -- Show an error message if a disabled key is pressed
-  local msg = [[<cmd>echohl Error | echo "KEY DISABLED" | echohl None<CR>]]
-
-  -- Disable arrow keys in insert mode with a styled message
-  vim.api.nvim_set_keymap("i", "<Up>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Down>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Left>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Right>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<Del>", "<C-o>" .. msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("i", "<BS>", "<C-o>" .. msg, { noremap = true, silent = false })
-
-  -- Disable arrow keys in normal mode with a styled message
-  vim.api.nvim_set_keymap("n", "<Up>", msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("n", "<Down>", msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("n", "<Left>", msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("n", "<Right>", msg, { noremap = true, silent = false })
-  vim.api.nvim_set_keymap("n", "<BS>", msg, { noremap = true, silent = false })
-end
+vim.keymap.set("n", "<c-z>", "<nop>", {
+    silent = true,
+    remap = true
+})

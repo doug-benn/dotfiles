a0351cfaeb640a599f1038ba3ecd71e6f6a6afaf
doug-benn <fiery.coder@gmail.com>
2026-06-10 20:41:44 +0100

    init

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.mouse = ""
+vim.opt.mousescroll = "ver:0,hor:0"
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.cmdheight = 1
+vim.opt.relativenumber = false
